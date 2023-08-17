f0b623bc0ac6103fb6d9d3bdf714690be0e7dc1e
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-08-17 15:10:56 -0400

    Add title to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <Head>
+        <title>OSHNN | Ocean State Health News Net</title>
+      </Head>
+      <h1>Hello world!</h1>
+    </>
+  );
 }
