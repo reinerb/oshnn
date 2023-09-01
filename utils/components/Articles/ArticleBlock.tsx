7a660c2aa16d6f735ef7f868990c129e59d61776
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-01 12:06:12 -0400

    Add paywall icon to ArticleBlock

--- utils/components/Articles/ArticleBlock.tsx
+++ utils/components/Articles/ArticleBlock.tsx
@@ -1,3 +1,5 @@
+import { faCircleDollarToSlot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import dayjs from "dayjs";
 import Link from "next/link";
 import React from "react";
@@ -18,7 +20,7 @@ function ArticleBlock(props: ArticleBlockProps) {
   return (
     <Link
       href={`/article/${slug}`}
-      className="group block flex h-full flex-col rounded-md border-2 border-blue-500 p-2 transition-colors duration-200 hover:border-blue-600 dark:hover:border-blue-400"
+      className="group flex h-full flex-col rounded-md border-2 border-blue-500 p-2 transition-colors duration-200 hover:border-blue-600 dark:hover:border-blue-400"
     >
       <h3 className="mb-1 block text-lg font-medium text-blue-500 transition-colors duration-200 group-hover:text-blue-600 dark:group-hover:text-blue-400">
         {title}
@@ -26,6 +28,11 @@ function ArticleBlock(props: ArticleBlockProps) {
       <div className="flex gap-1 divide-x divide-blue-700 leading-tight text-blue-700 dark:divide-blue-300 dark:text-blue-300">
         <span>{publicationTitle}</span>
         <span className="pl-1">{renderedDate}</span>
+        {paywall && (
+          <span className="pl-1">
+            <FontAwesomeIcon icon={faCircleDollarToSlot} />
+          </span>
+        )}
       </div>
     </Link>
   );
