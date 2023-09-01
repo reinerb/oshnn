904ceb3abbfe7a4dbfbe5be2f9d2e1375770f50e
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-01 11:44:37 -0400

    Add some styles to ArticleBlock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,48 @@
 import Head from "next/head";
 import PrimaryLayout from "@/utils/layouts/PrimaryLayout";
 import { wpQueryHandler } from "@/utils/queries/wpQueryHandler";
-import { WordPressData } from "@/utils/types/wordpressQueries";
-import Link from "next/link";
+import ArticleBlock from "@/utils/components/ArticleBlock";
+
+interface HomepageArticleProps {
+  id: number;
+  slug: string;
+  title: string;
+  publicationDate: string;
+  publicationTitle: string;
+  paywall: boolean;
+}
+
+interface HomepageProps {
+  posts: HomepageArticleProps[];
+}
 
 export async function getStaticProps() {
-  const posts = await wpQueryHandler("posts", {
-    fields: ["acf", "date", "categories"],
+  const postsQuery = await wpQueryHandler("posts", {
+    fields: ["acf", "date"],
+  });
+
+  const posts = postsQuery.map(({ acf, ...post }) => {
+    return {
+      ...post,
+      publicationDate: acf?.publicationDate,
+      publicationTitle: acf?.publicationTitle,
+      paywall: acf?.paywall,
+    };
   });
 
   return { props: { posts } };
 }
 
-export default function Home({ posts }: { posts: WordPressData[] }) {
+export default function Home({ posts }: HomepageProps) {
   return (
     <>
       <Head>
         <title>OSHNN | Ocean State Health News Net</title>
       </Head>
       <PrimaryLayout>
-        <Link href={`/article/${posts[0].slug}`}>{posts[0].title}</Link>
+        {posts.map(({ id, ...post }) => {
+          return <ArticleBlock key={id} {...post} />;
+        })}
       </PrimaryLayout>
     </>
   );
--- utils/components/ArticleBlock.tsx
+++ utils/components/ArticleBlock.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import Link from "next/link";
 import React from "react";
 
@@ -6,17 +7,23 @@ interface ArticleBlockProps {
   publicationTitle: string;
   publicationDate: string;
   slug: string;
+  paywall: boolean;
 }
 
 function ArticleBlock(props: ArticleBlockProps) {
-  const { title, publicationTitle, slug, publicationDate } = props;
+  const { title, publicationTitle, slug, publicationDate, paywall } = props;
+
+  const renderedDate = dayjs(publicationDate).format("MM/DD/YYYY");
 
   return (
-    <Link href={`/article/${slug}`}>
-      <h2 className="block">{title}</h2>
-      <div className="flex">
+    <Link
+      href={`/article/${slug}`}
+      className="block flex flex-col rounded-md border p-2"
+    >
+      <h2 className="mb-1 block text-lg text-blue-500">{title}</h2>
+      <div className="flex gap-1 divide-x divide-blue-700 leading-tight text-blue-700 dark:divide-blue-300 dark:text-blue-300">
         <span>{publicationTitle}</span>
-        <span>{publicationDate}</span>
+        <span className="pl-1">{renderedDate}</span>
       </div>
     </Link>
   );
