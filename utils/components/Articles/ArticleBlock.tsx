d41555dc6bbc1aabe023b82a9aad8bbeeb55a0e5
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-01 11:57:04 -0400

    Add group hover effect to ArticleBlock

--- utils/components/Articles/ArticleBlock.tsx
+++ utils/components/Articles/ArticleBlock.tsx
@@ -18,9 +18,11 @@ function ArticleBlock(props: ArticleBlockProps) {
   return (
     <Link
       href={`/article/${slug}`}
-      className="block flex h-full flex-col rounded-md border-2 border-blue-500 p-2"
+      className="group block flex h-full flex-col rounded-md border-2 border-blue-500 p-2 transition-colors duration-200 hover:border-blue-600 dark:hover:border-blue-400"
     >
-      <h3 className="mb-1 block text-lg font-medium text-blue-500">{title}</h3>
+      <h3 className="mb-1 block text-lg font-medium text-blue-500 transition-colors duration-200 group-hover:text-blue-600 dark:group-hover:text-blue-400">
+        {title}
+      </h3>
       <div className="flex gap-1 divide-x divide-blue-700 leading-tight text-blue-700 dark:divide-blue-300 dark:text-blue-300">
         <span>{publicationTitle}</span>
         <span className="pl-1">{renderedDate}</span>
