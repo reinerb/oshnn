07b8036c80b72e5b48f1c37812c373b4260830da
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-01 11:03:50 -0400

    Fix some color transitions

--- utils/components/ColorModeSwitcher.tsx
+++ utils/components/ColorModeSwitcher.tsx
@@ -19,7 +19,7 @@ function ColorModeSwitcher(props: Props) {
   return (
     <FontAwesomeIcon
       icon={theme === "dark" ? faMoon : faSun}
-      className={`cursor-pointer text-xl text-slate-950 hover:text-slate-800 dark:text-slate-50 dark:hover:text-slate-200 ${className}`}
+      className={`cursor-pointer text-xl text-slate-950 transition-colors duration-200 hover:text-slate-800 dark:text-slate-50 dark:hover:text-slate-200 ${className}`}
       onClick={toggleTheme}
     />
   );
--- utils/components/Header.tsx
+++ utils/components/Header.tsx
@@ -6,9 +6,13 @@ import PrimaryNav from "./PrimaryNav";
 function Header() {
   return (
     <header className="flex flex-col items-center px-8 py-4 md:flex-row md:justify-between">
-      <a href="/" id="logo" className="flex items-center gap-2">
+      <a
+        href="/"
+        id="logo"
+        className="flex items-center gap-2 transition-colors duration-200"
+      >
         <FontAwesomeIcon icon={faDroplet} className="text-3xl" />
-        <span className="text-4xl font-extrabold">OSHNN</span>
+        <span className="text-4xl font-extrabold ">OSHNN</span>
       </a>
       <PrimaryNav />
     </header>
