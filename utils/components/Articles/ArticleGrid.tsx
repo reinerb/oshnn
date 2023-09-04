555fc953940ded08edc05f199ee8042615ecefe2
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-03 23:59:21 -0400

    Make ArticleGrid number of cols responsive

--- utils/components/Articles/ArticleGrid.tsx
+++ utils/components/Articles/ArticleGrid.tsx
@@ -9,7 +9,9 @@ function ArticleGrid(props: ArticleGridProps) {
   const { title, children } = props;
 
   return (
-    <section className={`grid grid-cols-4 gap-4`}>
+    <section
+      className={`grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4`}
+    >
       <h2 className="col-span-full text-xl">{title}</h2>
       {children}
     </section>
