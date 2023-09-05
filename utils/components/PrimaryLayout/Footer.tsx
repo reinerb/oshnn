a46ed86155964d4dfa50e146b6107c9e2a99257d
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-05 14:53:13 -0400

    Change copyright icon in footer

--- utils/components/PrimaryLayout/Footer.tsx
+++ utils/components/PrimaryLayout/Footer.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 
 function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center gap-1 px-8 py-4 text-center text-sm sm:text-base md:flex-row md:gap-2 md:divide-x xl:gap-4">
-      <p>Copyright &copy; 2023 OSHNN</p>
+      <p>
+        Copyright <FontAwesomeIcon icon={faCopyright} /> 2023 OSHNN
+      </p>
       <Link href="/privacy" className="md:pl-2 xl:pl-4">
         Privacy Policy
       </Link>
