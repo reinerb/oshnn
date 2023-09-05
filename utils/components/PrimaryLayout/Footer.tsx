d88bdedb2fd37c7edeb13d3604b282a323d059c7
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-05 13:00:36 -0400

    Add content to footer

--- utils/components/PrimaryLayout/Footer.tsx
+++ utils/components/PrimaryLayout/Footer.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
 import React from "react";
 
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="flex justify-center gap-4 divide-x px-8 py-4">
+      <p>Copyright &copy; 2023 OSHNN</p>
+      <Link href="/privacy" className="pl-4">
+        Privacy Policy
+      </Link>
+      <Link href="/disclaimer" className="pl-4">
+        Disclaimer
+      </Link>
+      <p className="pl-4">
+        Site built by{" "}
+        <Link href="https://masscommunications.co/">
+          Mass Communications Concepts, LLC
+        </Link>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
