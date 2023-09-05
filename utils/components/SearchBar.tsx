7354c59dc728cb80379382a44af9d081ea297a18
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-05 12:47:13 -0400

    Style search bar

--- utils/components/SearchBar.tsx
+++ utils/components/SearchBar.tsx
@@ -15,7 +15,7 @@ const SearchBar = ({ action, initialQuery = "" }: SearchBarProps) => {
   };
 
   return (
-    <section className="grid">
+    <section className="flex flex-col gap-2 sm:flex-row">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
@@ -24,8 +24,14 @@ const SearchBar = ({ action, initialQuery = "" }: SearchBarProps) => {
         value={query}
         onChange={handleChange}
         placeholder="Search"
+        className="rounded-md bg-slate-200 px-4 py-2 outline-none placeholder:text-slate-800 focus-within:bg-slate-300 dark:bg-slate-800 dark:placeholder:text-slate-200 dark:focus-within:bg-slate-700 sm:flex-1"
       />
-      <button onClick={() => action(query)}>Search</button>
+      <button
+        onClick={() => action(query)}
+        className="rounded-md bg-blue-500 px-4 py-2 text-slate-50 transition-colors duration-200 hover:bg-blue-600 active:bg-blue-700 dark:hover:bg-blue-400 dark:active:bg-blue-300"
+      >
+        Search
+      </button>
     </section>
   );
 };
