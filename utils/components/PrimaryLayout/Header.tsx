823a0a5ca5f3d71523efd208e2b4d3d93df063ca
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-07 10:05:48 -0400

    Update logo SVG path

--- utils/components/PrimaryLayout/Header.tsx
+++ utils/components/PrimaryLayout/Header.tsx
@@ -3,17 +3,14 @@
 import React from "react";
 import PrimaryNav from "../PrimaryNav";
 import Logo from "../Logo";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="flex flex-col items-center px-8 py-4 md:flex-row md:justify-between">
-      <a
-        href="/"
-        id="logo"
-        className="flex items-center gap-2 transition-colors duration-200"
-      >
-        <Logo />
-      </a>
+      <Link href="/" id="logo">
+        <Logo className="hover:fill-primary-600 active:fill-primary-700 dark:hover:fill-primary-400 dark:active:fill-primary-300" />
+      </Link>
       <PrimaryNav />
     </header>
   );
