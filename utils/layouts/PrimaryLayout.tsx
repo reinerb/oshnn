7cb816265a19e61f14e11e48238b3d2adf1ca085
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-18 16:49:07 -0400

    Add title and meta props

--- utils/layouts/PrimaryLayout.tsx
+++ utils/layouts/PrimaryLayout.tsx
@@ -2,16 +2,28 @@ import React from "react";
 import Header from "../components/PrimaryLayout/Header";
 import Footer from "../components/PrimaryLayout/Footer";
 import FooterDivider from "../components/PrimaryLayout/FooterDivider";
+import Head from "next/head";
 
 type PrimaryLayoutProps = {
+  title: string;
+  meta?: string;
   children?: React.ReactNode;
   className?: string;
 };
 
-function PrimaryLayout({ children, className }: PrimaryLayoutProps) {
+function PrimaryLayout({
+  title,
+  meta,
+  children,
+  className,
+}: PrimaryLayoutProps) {
   return (
     <>
-      <div className={`grid-rows-body grid min-h-screen`}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={meta} />
+      </Head>
+      <div className={`grid min-h-screen grid-rows-body`}>
         <Header />
         <div className="flex justify-center">
           <main className={`container mx-8 ${className}`}>{children}</main>
