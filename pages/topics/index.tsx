cf23b9ee582206e1b58e4a4fc4ccb41ec3132ee4
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-18 16:02:47 -0400

    Abstract TopicsLayout

--- pages/topics/index.tsx
+++ pages/topics/index.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import TopicButtons from "@/utils/components/Topics/TopicButtons";
+import TopicsLayout from "@/utils/layouts/TopicsLayout";
 
 const PER_PAGE = 12;
 
@@ -29,42 +30,12 @@ function TopicPage({ allTopics, posts, totalPages }: BlockPageProps) {
   const hasNextPage = totalPages! > 1;
 
   return (
-    <PrimaryLayout className="grid auto-rows-max gap-4 lg:grid-cols-3">
-      <section className="grid auto-rows-max grid-cols-1 gap-4 lg:col-span-2 xl:grid-cols-2">
-        <h1 className="col-span-full text-2xl">Articles</h1>
-        {posts.map((post) => (
-          <ArticleBlock {...post} key={post.id} />
-        ))}
-        <div className="col-span-full flex"></div>
-        {hasNextPage && (
-          <div className="col-span-full flex justify-end">
-            <Link href="/topics/page/2">
-              Next <FontAwesomeIcon icon={faAngleRight} />
-            </Link>
-          </div>
-        )}
-      </section>
-      <nav className="flex flex-col items-center gap-4 border-l-0 border-t pt-4 lg:items-start lg:border-l lg:border-t-0 lg:pl-4 lg:pt-0">
-        <div className="flex flex-col items-center gap-2 lg:items-start">
-          <h2 className="text-lg">Rhode Island Topics</h2>
-          <TopicButtons
-            topic={allTopics.rhodeIsland}
-            className="lg:justify-start"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-2 lg:items-start">
-          <h2 className="text-lg">National Topics</h2>
-          <TopicButtons
-            topic={allTopics.national}
-            className="lg:justify-start"
-          />
-        </div>
-      </nav>
-      <SearchBar
-        action={(query) => console.log(query)}
-        className="col-span-full"
-      />
-    </PrimaryLayout>
+    <TopicsLayout
+      page={1}
+      totalPages={totalPages}
+      allTopics={allTopics}
+      posts={posts}
+    />
   );
 }
 
---
+++ utils/layouts/TopicsLayout.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import PrimaryLayout from "./PrimaryLayout";
+import { Category } from "../types/blog";
+import { BlockPageProps } from "../types/BlogPages";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import ArticleBlock from "../components/Articles/ArticleBlock";
+import { twMerge } from "tailwind-merge";
+import SearchBar from "../components/SearchBar";
+import TopicButtons from "../components/Topics/TopicButtons";
+
+type TopicsLayoutProps = BlockPageProps & {
+  className?: string;
+};
+
+function TopicsLayout({
+  topic,
+  allTopics,
+  posts,
+  page,
+  totalPages,
+  className,
+}: TopicsLayoutProps) {
+  const hasNextPage = page && totalPages && page < totalPages;
+  const hasPreviousPage = page && page > 1;
+
+  return (
+    <PrimaryLayout className="grid auto-rows-max gap-4 lg:grid-cols-3">
+      <section className="grid auto-rows-max grid-cols-1 gap-4 lg:col-span-2 xl:grid-cols-2">
+        <h1 className="col-span-full text-2xl">Articles</h1>
+        {posts.map((post) => (
+          <ArticleBlock {...post} key={post.id} />
+        ))}
+        <div className="col-span-full flex"></div>
+        {(hasNextPage || hasPreviousPage) && (
+          <div
+            className={twMerge(
+              "col-span-full flex",
+              hasNextPage && hasPreviousPage
+                ? "justify-between"
+                : hasNextPage
+                ? "justify-end"
+                : "justify-start",
+            )}
+          >
+            {hasPreviousPage && (
+              <Link href={`/topics/page/${page - 1}`}>
+                Previous <FontAwesomeIcon icon={faAngleLeft} />
+              </Link>
+            )}
+            {hasNextPage && (
+              <Link href={`/topics/page/${page + 1}`}>
+                Next <FontAwesomeIcon icon={faAngleRight} />
+              </Link>
+            )}
+          </div>
+        )}
+      </section>
+      <nav className="flex flex-col items-center gap-4 border-l-0 border-t pt-4 lg:items-start lg:border-l lg:border-t-0 lg:pl-4 lg:pt-0">
+        <div className="flex flex-col items-center gap-2 lg:items-start">
+          <h2 className="text-lg">Rhode Island Topics</h2>
+          <TopicButtons
+            topic={allTopics.rhodeIsland}
+            className="lg:justify-start"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2 lg:items-start">
+          <h2 className="text-lg">National Topics</h2>
+          <TopicButtons
+            topic={allTopics.national}
+            className="lg:justify-start"
+          />
+        </div>
+      </nav>
+      <SearchBar
+        action={(query) => console.log(query)}
+        className="col-span-full"
+      />
+    </PrimaryLayout>
+  );
+}
+
+export default TopicsLayout;
