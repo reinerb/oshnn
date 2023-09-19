cd1f0aa6200ac64811b1c826b05cfb166ee651d5
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-19 10:48:16 -0400

    Add 301 redirects for /article, /*/page/1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/article",
+        destination: "/topics",
+        permanent: true,
+      },
+      {
+        source: "/topics/page/1",
+        destination: "/topics",
+        permanent: true,
+      },
+      {
+        source: "/topics/:slug/page/1",
+        destination: "/topics/:slug",
+        permanent: true,
+      },
+    ];
+  },
+};
