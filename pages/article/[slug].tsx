e6f6206d6d2f8ab583e29cdca8c744a3f2d37a7a
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-19 13:57:15 -0400

    Add related posts section to article pages

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Link from "next/link";
-import PrimaryLayout from "@/utils/layouts/PrimaryLayout";
-import { wpQueryHandler } from "@/utils/queries/wpQueryHandler";
+import dynamic from "next/dynamic";
+import dayjs from "dayjs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleDollarToSlot } from "@fortawesome/free-solid-svg-icons";
-import dayjs from "dayjs";
-import dynamic from "next/dynamic";
+import PrimaryLayout from "@/utils/layouts/PrimaryLayout";
+import { wpQueryHandler } from "@/utils/queries/wpQueryHandler";
 import LinkButton from "@/utils/components/LinkButton";
+import { getPosts } from "@/utils/queries/blogPageHandlers";
+import ArticleGrid from "@/utils/components/Articles/ArticleGrid";
+import ArticleBlock from "@/utils/components/Articles/ArticleBlock";
+import type { GetStaticPaths, GetStaticProps } from "next";
 import type { PostData } from "@/utils/types/wordpressQueries";
 import type { Category } from "@/utils/types/blog";
-import type { GetStaticPaths, GetStaticProps } from "next";
+import type { BlockArticle } from "@/utils/types/BlogPages";
 
 const ShareButtons = dynamic(
   () => import("@/utils/components/ShareButtons/ShareButtons"),
@@ -18,7 +22,7 @@ const ShareButtons = dynamic(
   },
 );
 
-interface ArticleProps {
+type ArticleProps = {
   title: string;
   content: string;
   publicationDate: string;
@@ -27,24 +31,47 @@ interface ArticleProps {
   articleUrl: string;
   paywall: boolean;
   categories: Category[];
-}
+  relatedPosts: BlockArticle[];
+};
 
 export const getStaticProps: GetStaticProps<ArticleProps> = async (context) => {
   const slug = context.params?.slug as string;
 
+  // Query this post's info from the CMS
   const response = (await wpQueryHandler("posts", {
     fields: ["acf", "categories", "content"],
     slug,
   })) as PostData[];
   const postData = response[0];
 
+  // Get all of the categories for this post
   const categoryQuery = (await wpQueryHandler("categories")) as Category[];
-
   const categories = categoryQuery.filter((category) => {
     return postData.categories?.includes(category.id);
   });
 
+  // Sort them so National and Rhode Island come first (they have the lowest ID)
   categories.sort((a, b) => a.id - b.id);
+
+  // Find related posts: the newest posts that share a category with this one
+  // Don't include general categories - National and Rhode Island
+  // Ask for one more than needed - important in next step
+  const relatedPostsQuery = await getPosts({
+    page: 1,
+    perPage: 4,
+    categoryIds: categories
+      .filter(
+        (category) =>
+          category.title !== "National" && category.title !== "Rhode Island",
+      )
+      .map((category) => category.id),
+  });
+
+  // Remove this post from the list, if present
+  // Then get the first 3 elements to display
+  const relatedPosts = relatedPostsQuery
+    .filter((post) => post.id !== postData.id)
+    .slice(0, 3);
 
   return {
     props: {
@@ -52,6 +79,7 @@ export const getStaticProps: GetStaticProps<ArticleProps> = async (context) => {
       content: postData.content,
       ...postData.acf,
       categories,
+      relatedPosts,
     },
   };
 };
@@ -73,7 +101,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-function Page({
+function ArticlePage({
   title,
   content,
   publicationDate,
@@ -82,12 +110,13 @@ function Page({
   articleUrl,
   articleAuthors,
   categories,
+  relatedPosts,
 }: ArticleProps) {
   const renderedDate = dayjs(publicationDate).format("MMMM D, YYYY");
 
   const categoryLinks = categories.map((category, index) => [
     index > 0 && ", ",
-    <Link key={category.id} href={`/categories/${category.slug}`}>
+    <Link key={category.id} href={`/topics/${category.slug}`}>
       {category.title}
     </Link>,
   ]);
@@ -100,14 +129,22 @@ function Page({
       meta={content}
       className="flex flex-col gap-4"
     >
-      <article className="flex flex-col gap-2">
-        <h1 className="text-2xl">{title}</h1>
-        <div className="flex flex-col gap-1 italic sm:flex-row sm:gap-4 sm:divide-x sm:not-italic">
-          <span>{publicationTitle}</span>
-          <span className="sm:pl-4">{articleAuthors}</span>
-          <span className="sm:pl-4">{renderedDate}</span>
+      <article className="flex flex-col gap-4">
+        {/* Blog content */}
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl">{title}</h1>
+          <div className="flex flex-col gap-1 italic sm:flex-row sm:gap-4 sm:divide-x sm:not-italic">
+            <span>{publicationTitle}</span>
+            <span className="sm:pl-4">{articleAuthors}</span>
+            <span className="sm:pl-4">{renderedDate}</span>
+          </div>
         </div>
-        <section dangerouslySetInnerHTML={{ __html: content }} />
+        <section
+          dangerouslySetInnerHTML={{ __html: content }}
+          className="flex gap-2"
+        />
+
+        {/* Link to full article */}
         <LinkButton
           target="_blank"
           rel="noreferrer noopener"
@@ -119,14 +156,24 @@ function Page({
             <FontAwesomeIcon icon={faCircleDollarToSlot} className="ml-2" />
           )}
         </LinkButton>
+
+        {/* Topics list */}
         <div>Topics: {categoryLinks}</div>
       </article>
+
+      {/* Social media share */}
       <ShareButtons callToAction="Share This Article" quote={title} />
+
+      {/* Related articles */}
       <section>
-        <h2 className="text-xl">Related posts</h2>
+        <ArticleGrid title="Related headlines" headlineStart>
+          {relatedPosts.map(({ id, ...post }) => (
+            <ArticleBlock {...post} key={id} />
+          ))}
+        </ArticleGrid>
       </section>
     </PrimaryLayout>
   );
 }
 
-export default Page;
+export default ArticlePage;
--- utils/components/Articles/ArticleGrid.tsx
+++ utils/components/Articles/ArticleGrid.tsx
@@ -2,17 +2,31 @@ import { Category } from "@/utils/types/blog";
 import React from "react";
 import LinkButton from "../LinkButton";
 import TopicButtons from "../Topics/TopicButtons";
+import { twMerge } from "tailwind-merge";
 
 type ArticleGridProps = {
   title: string;
   children: React.ReactNode;
   topic?: Category;
+  headlineStart?: boolean;
 };
 
-function ArticleGrid({ title, children, topic }: ArticleGridProps) {
+function ArticleGrid({
+  title,
+  children,
+  topic,
+  headlineStart,
+}: ArticleGridProps) {
   return (
     <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      <h2 className="col-span-full text-center text-xl">{title}</h2>
+      <h2
+        className={twMerge(
+          "col-span-full text-xl",
+          headlineStart ? "text-start" : "text-center",
+        )}
+      >
+        {title}
+      </h2>
       {children}
       {topic?.children && (
         <div className="flex flex-col flex-wrap gap-2 rounded-md bg-neutral-100 p-2 dark:bg-neutral-900">
