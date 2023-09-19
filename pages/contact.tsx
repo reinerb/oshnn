f2affdb9286b765b88b3c0ed41a6f47db315bdb2
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-19 15:00:04 -0400

    Add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import PrimaryLayout from "@/utils/layouts/PrimaryLayout";
+import React from "react";
+
+function ContactPage() {
+  return (
+    <PrimaryLayout title="Contact Us | OSHNN">
+      <h1 className="text-2xl">Contact Us</h1>
+    </PrimaryLayout>
+  );
+}
+
+export default ContactPage;
