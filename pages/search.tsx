e135a8961b0e99bb8ca277896c5a5580b3e01f27
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-21 11:11:08 -0400

    Add search page

---
+++ pages/search.tsx
@@ -0,0 +1,13 @@
+import Search from "@/utils/components/Search/Search";
+import PrimaryLayout from "@/utils/layouts/PrimaryLayout";
+import React from "react";
+
+function SearchPage() {
+  return (
+    <PrimaryLayout title="Search | OSHNN">
+      <Search />
+    </PrimaryLayout>
+  );
+}
+
+export default SearchPage;
--- utils/components/Search/Search.tsx
+++ utils/components/Search/Search.tsx
@@ -4,15 +4,11 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
 
-type SearchProps = {
-  query: string;
-};
-
-function Search({ query }: SearchProps) {
+function Search() {
   return (
     <>
       <SearchBar action={(query) => console.log(query)} />;
-      <SearchResults query={query} />
+      <SearchResults query={""} />
     </>
   );
 }
