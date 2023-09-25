fc508dbc8bc367ea58cecbcfb5d6b8b842380d08
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-25 11:50:34 -0400

    Add tracking script to head of document on prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,6 +22,24 @@ export default function Document() {
           sizes="16x16"
           href="/favicon/favicon-16x16.png"
         />
+        {process.env.NEXT_PUBLIC_ENV === "production" && (
+          <>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-PZ8B6SSL2T"
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PZ8B6SSL2T');
+            `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <body>
         <Main />
