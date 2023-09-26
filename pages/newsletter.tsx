143a4ccb1136cb10c15abef6ccb127d170676c8d
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-26 13:26:13 -0400

    Remove unnecessary imports

--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -1,9 +1,6 @@
 import NewsletterForm from "@/utils/components/NewsletterForm";
 import PrimaryLayout from "@/utils/layouts/PrimaryLayout";
-import { ReCaptchaProvider } from "next-recaptcha-v3";
 import React from "react";
-
-const RECAPTCHA_SITE_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
 
 function NewsletterSignupPage() {
   return (
