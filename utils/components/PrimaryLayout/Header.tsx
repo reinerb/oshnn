8fe193517c0cf9dbd2f18a818c4bf1b91e009e22
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-28 12:27:32 -0400

    Add responsive nav menu

--- utils/components/PrimaryLayout/Header.tsx
+++ utils/components/PrimaryLayout/Header.tsx
@@ -1,7 +1,6 @@
 // import { faDroplet } from "@fortawesome/free-solid-svg-icons";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import PrimaryNav from "./PrimaryNav";
 import Logo from "../Logo";
 import Link from "next/link";
 import dynamic from "next/dynamic";
@@ -10,16 +9,15 @@ const ColorModeSwitcher = dynamic(() => import("../ColorModeSwitcher"), {
   ssr: false,
 });
 
+const PrimaryNav = dynamic(() => import("./PrimaryNav"), { ssr: false });
+
 function Header() {
   return (
-    <header className="relative flex flex-col items-center gap-4 px-8 py-4 md:flex-row md:justify-between">
+    <header className="relative flex flex-row items-center justify-between gap-4 px-8 py-4">
       <Link href="/" id="logo" aria-label="home">
         <Logo className="hover:fill-secondary-600 active:fill-secondary-700 dark:hover:fill-secondary-400 dark:active:fill-secondary-300" />
       </Link>
-      <div className="flex items-center gap-4">
-        <PrimaryNav />
-        <ColorModeSwitcher />
-      </div>
+      <PrimaryNav />
     </header>
   );
 }
--- utils/components/PrimaryLayout/PrimaryNav.tsx
+++ utils/components/PrimaryLayout/PrimaryNav.tsx
@@ -1,21 +1,99 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { useWindowSize } from "@uidotdev/usehooks";
+import ColorModeSwitcher from "../ColorModeSwitcher";
+import { twMerge } from "tailwind-merge";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+const links: NavLink[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Newsletter",
+    href: "/newsletter",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
 
 function PrimaryNav() {
-  return (
-    <nav>
-      <ul className="flex flex-row items-center gap-4 font-semibold">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/newsletter">Newsletter Signup</a>
-        </li>
-      </ul>
-    </nav>
+  const { width } = useWindowSize();
+  const [active, setActive] = useState(false);
+
+  const mdNav = (
+    <div className="flex items-center gap-4">
+      <nav>
+        <ul className="flex flex-row items-center gap-4 font-semibold">
+          {links.map(({ name, href }) => (
+            <li key={href}>
+              <a href={href}>{name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <ColorModeSwitcher />
+    </div>
   );
+
+  const xsNav = (
+    <>
+      <button
+        className="self-start text-2xl text-neutral-950 transition-colors duration-200 hover:text-neutral-800 active:text-neutral-700 dark:text-neutral-50 dark:hover:text-neutral-200 dark:active:text-neutral-300"
+        aria-label="Open navigation"
+        onClick={() => setActive(true)}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <div
+        aria-hidden
+        className={twMerge(
+          "fixed left-0 top-0 h-screen w-screen bg-neutral-950 bg-opacity-50 transition-opacity duration-200",
+          active ? "opacity-100" : "pointer-events-none opacity-0",
+        )}
+        onClick={() => setActive(false)}
+      />
+      <div
+        className={twMerge(
+          "fixed right-0 top-0 z-50 flex h-screen flex-col gap-4 bg-neutral-200 px-8 py-4 transition-transform duration-200 dark:bg-neutral-800",
+          active ? "translate-x-0" : "translate-x-full",
+        )}
+      >
+        <div className="flex justify-between">
+          <ColorModeSwitcher className="text-lg" />
+          <button
+            aria-label="Close navigation"
+            onClick={() => setActive(false)}
+          >
+            <FontAwesomeIcon icon={faX} className="text-lg" />
+          </button>
+        </div>
+        <ul className="flex flex-col items-center gap-2">
+          {links.map(({ name, href }) => (
+            <li key={href}>
+              <a href={href}>{name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+
+  return width && width >= 768 ? mdNav : xsNav;
 }
 
 export default PrimaryNav;
